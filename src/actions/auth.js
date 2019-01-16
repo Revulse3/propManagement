34d6dd20ab62567dc209d84d0c03678d7dff06d9
Revulse3 <dustynfoster@aol.com>
2019-01-15 20:06:43 -0700

    handled signup post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,15 +2,16 @@ import axios from 'axios';
 
 import {ROOT_URL} from '../config';
 
-export funtion Signup(fields, success) {
+export function signUp(fields, success) {
     console.log(fields);
     return function(dispatch) {
-        // axios.post(`${ROOT_URL}/signUp`, fields)
-        //     .then(response => {
-        //         //do something
-        //     })
-        //     .catch(err => {
-        //         if(err) { console.log(err) }
-        //     })
+        axios.post(`${ROOT_URL}/signUp`, fields)
+            .then(response => {
+                console.log(response);
+                success()
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
     }
 }
